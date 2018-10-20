3e4983389024fbef6e921d731072bcc97f718070
m.hama <m.hama@mibel.cs.tsukuba.ac.jp>
2018-10-20 17:25:58 +0900

    update dotfiles

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# shells
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# vim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+# git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+
+# tmux
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# ssh
+mkdir -p ~/.ssh
+ln -sf ~/dotfiles/.ssh/config ~/.ssh/config 
