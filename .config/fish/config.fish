3e65a0216fa483b492b97491dbf2eceb0a348ffb
m.hama <masakatsu.hamashita@gunosy.com>
2020-12-11 19:20:00 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set -x WORKON_HOME $HOME/.virtualenvs
 
+set fish_color_command green
+
 set -x LANG ja_JP.UTF-8
 set -x LC_CTYPE ja_JP.UTF-8
 set -x LC_ALL 'ja_JP.UTF-8'
@@ -23,6 +25,28 @@ function _command_exists
     which $1 &> /dev/null
 end
 
+# .bash_profile
+set -x PYENV_ROOT "$HOME/.pyenv"
+set -x PATH $PYENV_ROOT/bin:$PATH
+set -x PATH "$HOME/.pyenv/shims:$PATH"
+set -x PYTHONSTARTUP ~/.pythonstartup
+set -x PATH /usr/local/bin:$PATH
+set -x SDKMAN_DIR "/Users/masakatsu.hamashita/.sdkman"
+# .bashrc
+set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
+set -x PYENV_ROOT "$HOME/.pyenv"
+set -x PATH $PYENV_ROOT/shims:$PATH
+set -x PATH $HOME/.nodebrew/current/bin:$PATH
+set -x CPLUS_INCLUDE_PATH $CPLUS_INCLUDE_PATH:/usr/local/include/c++/9.2.0/x86_64-apple-darwin18/
+if [ -d "$PYENV_ROOT" ]
+       set -x PATH $PYENV_ROOT/bin:$PATH
+       source (pyenv init - | psub)
+    source (pyenv virtualenv-init - | psub)
+end
+
+set -x PATH "$HOME/bin:$PATH"
+set -U fish_user_paths $fish_user_paths $HOME/.cargo/bin
+
 #neovimのためのpath
 set -x XDG_CONFIG_HOME "~/.config"
 
@@ -32,47 +56,56 @@ set -x LSCOLORS DxGxcxdxCxegedabagacad
 
 
 # git checkout branchをfzfで選択
-alias co='git checkout (git branch -a | tr -d " " |fzf --height 100% --prompt "CHECKOUT BRANCH>" --preview "git log --color=always {}" | head -n 1 | sed -e "s/^\*\s*//g" | perl -pe "s/remotes\/origin\///g")'
+alias co 'git checkout (git branch -a | tr -d " " |fzf --height 100% --prompt "CHECKOUT BRANCH>" --preview "git log --color=always {}" | head -n 1 | sed -e "s/^\*\s*//g" | perl -pe "s/remotes\/origin\///g")'
 
-alias :q='exit'
-alias gd='git diff'
-alias ga='git add'
-alias gp='git pull'
-alias gc='git commit'
-alias gis='git status'
-alias ...='cd ../../'
-alias ref='source ~/.zshrc'
+alias :q 'exit'
+alias gd 'git diff'
+alias ga 'git add'
+alias gp 'git pull'
+alias gc 'git commit'
+alias gis 'git status'
+alias ... 'cd ../../'
+alias ref 'source ~/.zshrc'
 
-# alias gcl='gcloud compute ssh --zone us-west1-b pytorch-study-vm'
-alias gcl='gcloud beta compute ssh --zone "us-west1-b" "global-wheat-detection-vm" --project "euphoric-diode-279610" -- -L 8080:localhost:8080 -L 8081:localhost:8081'
+alias gcl 'gcloud beta compute ssh --zone "us-west1-b" "global-wheat-detection-vm" --project "euphoric-diode-279610" -- -L 8080:localhost:8080 -L 8081:localhost:8081'
 # alias cd='pushd'
 # alias cdd='popd'
-alias df='df -h'
-alias vi='vim'
-alias diff='colordiff'
-alias his='history | fzf | awk -F ";" "{system($2)}"'
-alias del_swap='rm ~/.local/share/nvim/swap/*'
-_command_exists rmtrash || alias rm='rm -i'
-! _command_exists rmtrash || alias rm='rmtrash' #実際はmv2trash(renameしてる)
-! _command_exists nvim || alias vim='nvim'
-! _command_exists gcc-10 || alias gcc='/usr/local/bin/gcc-10'
-! _command_exists g++-10 || alias g++='/usr/local/bin/g++-10'
-# ! _command_exists gcloud || alias gcl='gcloud compute ssh --zone us-west1-b "pytorch-study-vm" -- -L 8888:localhost:8888'
-# ! _command_exists gcloud || alias gcli='gcloud compute ssh --zone us-central1-a "instance-2" -- -L 8888:localhost:8888'
-! _command_exists tmux || alias ta='tmux a -d'
-if [ (command -v exa) ]
-    alias ls='exa -a --git'
-    alias tree='exa --tree'
+alias df 'df -h'
+alias vi 'vim'
+alias diff 'colordiff'
+alias his 'history | fzf | awk -F ";" "{system($2)}"'
+alias del_swap 'rm ~/.local/share/nvim/swap/*'
+if [ (command -v rmtrash) ]
+    alias rm 'rmtrash'
 else
-    alias ls='ls -at'
+    alias rm 'rm -i'
+end
+if [ (command -v nvim) ]
+    alias vim 'nvim'
+end
+if [ (command -v gcc-10) ]
+    alias gcc '/usr/local/bin/gcc-10'
+end
+if [ (command -v g++-10) ]
+    alias g++ '/usr/local/bin/g++-10'
+end
+if [ (command -v tmux) ]
+    alias ta 'tmux a -d'
 end
 
-alias grep='grep --color'
-if [ -e "/Applications/CotEditor.app" ]
-  alias cot='open -a /Applications/'\''CotEditor.app'\'''
+if [ (command -v exa) ]
+    alias ls 'exa -a --git'
+    alias tree 'exa --tree'
+else
+    alias ls 'ls -at'
 end
-#alias history='history -f'
-alias history='cat ~/.zhistory'
+
+alias grep 'grep --color'
+if [ -e "/Applications/CotEditor.app" ]
+  alias cot 'open -a /Applications/'\''CotEditor.app'\'''
+end
+# alias history='history -f'
+# alias history 'cat ~/.zhistory'
 # cd, pwd などは history に登録しない
 set -x HISTORY_IGNORE "(cd|pwd|ls)"
 
@@ -98,7 +131,7 @@ set -x LESS '-R'
 # 補完
 
 # fzf-cdr
-alias cdd='fzf-cdr'
+alias cdd 'fzf-cdr'
 function fzf-cdr
     set target_dir `cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf`
     set target_dir `echo $target_dir/\~/$HOME`
@@ -107,26 +140,12 @@ function fzf-cdr
     end
 end
 
-## z の設定
-sh (brew --prefix)/etc/profile.d/z.sh
-function precmd
-    # z の設定
-    _z --add "(pwd -P)"
-
-    # https://qiita.com/arks22/items/db8eb6a14223ce29219a
-    if [ ! -z $TMUX ]
-      tmux refresh-client -S
-    end
-end
 
 # cdr の設定
 if [ ! -e  "$HOME/.cache/shell/chpwd-recent-dirs" ]
   mkdir -p $HOME/.cache/shell/
   touch $HOME/.cache/shell/chpwd-recent-dirs
 end
-
-
-
 
 
 # fshow - git commit browser
