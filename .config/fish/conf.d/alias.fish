fcaf7a04ddd32842e9e9e9ca01d052f909622399
m.hama <masakatsu.hamashita@gunosy.com>
2021-05-02 11:31:37 +0900

    Update alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -22,6 +22,7 @@ alias diff 'colordiff'
 alias his 'history | fzf | awk -F ";" "{system($2)}"'
 alias del_swap 'rm ~/.local/share/nvim/swap/*'
 alias root 'cd (git rev-parse --show-toplevel)'
+alias gg 'open https://github.(git config remote.origin.url | cut -f2 -d. | tr ':' /)'
 
 # abbr
 abbr gd git diff
@@ -34,7 +35,6 @@ abbr -a awsdoc "aws ecr get-login-password | docker login --username AWS --passw
 abbr -a one onelogin-aws-login -d 32400 --config-name ads --username masakatsu.hamashita@gunosy.com --profile default
 abbr -a oneads onelogin-aws-login -d 32400 --config-name ads --username masakatsu.hamashita@gunosy.com --profile ads
 # abbr -a del "git branch --merged | grep -vE '^\\*|master|develop|staging' | xargs -I % git branch -d % && git remote prune origin"
-abbr -a gh open https://github.(git config remote.origin.url | cut -f2 -d. | tr ':' /)
 
 
 if [ (command -v rmtrash) ]
