479ea6a82eaed951c49a0b6beb2479ab749d3d2a
m.hama <masakatsu.hamashita@gunosy.com>
2021-05-21 01:33:42 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# tpm install 
+# tpm install
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # neovim install
 if [ "$(uname)" == 'Darwin' ]; then
     brew install neovim
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    curl -L https://github.com/neovim/neovim/releases/download/v0.4.0/nvim.appimage -o nvim
+    curl -L https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage -o nvim
     mkdir -p ~/local/bin/
     mv ./nvim ~/local/bin/nvim
     rm nvim
@@ -18,7 +18,10 @@ cd ~/.cache/dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh
 
-# zplug install
-# curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
+# fish install
+brew install fish
+echo "Please add `/usr/local/bin/fish` to /etc/shells "
 # font install
+echo "Please install powerline font"
+echo "Font: menlo for powerline"
+echo "Non-ASCII: Hack Nerd Font Mono for defx.nvim"
