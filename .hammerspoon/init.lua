606a47b29995b917d33bd51a4e3c7a00f7541d90
m-hamashita <masakatsu.hamashita@gunosy.com>
2021-11-27 00:28:20 +0900

    Refactor hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,4 @@
 -- https://github.com/ipatch/dotfiles/blob/master/jobs/macos/Users/mr-fancy/hammerspoon/ctrlDoublePress.lua
-local alert    = require("hs.alert")
 local timer    = require("hs.timer")
 local eventtap = require("hs.eventtap")
 
@@ -8,10 +7,10 @@ local events   = eventtap.event.types
 local module   = {}
 local spaces = require("hs._asm.undocumented.spaces")
 
--- how quickly must the two single ctrl taps occur?
+-- double tap の間隔[s]
 module.timeFrame = 1
 
--- what to do when the double tap of ctrl occurs
+-- double tap で toggle で kitty を表示/非表示する
 module.action = function()
     local appName = "kitty"
     local app = hs.application.get(appName)
@@ -46,7 +45,7 @@ end)
 
 
 local timeFirstControl, firstDown, secondDown = 0, false, false
--- verify that no keyboard flags are being pressed
+
 local noFlags = function(ev)
     local result = true
     for k,v in pairs(ev:getFlags()) do
@@ -58,7 +57,7 @@ local noFlags = function(ev)
     return result
 end
 
--- verify that *only* the ctrl key flag is being pressed
+-- control だけ押されているか確認. 例えば shift+control 等は無視するようにする
 local onlyCtrl = function(ev)
     local result = ev:getFlags().ctrl
     for k,v in pairs(ev:getFlags()) do
@@ -70,30 +69,26 @@ local onlyCtrl = function(ev)
     return result
 end
 
--- the actual workhorse
-
+-- module.timeFrame 秒以内に2回 ctrl を押したらダブルタップとみなす
 module.eventWatcher = eventtap.new({events.flagsChanged, events.keyDown}, function(ev)
-    -- if it's been too long; previous state doesn't matter
     if (timer.secondsSinceEpoch() - timeFirstControl) > module.timeFrame then
         timeFirstControl, firstDown, secondDown = 0, false, false
     end
 
     if ev:getType() == events.flagsChanged then
-        if noFlags(ev) and firstDown and secondDown then -- ctrl up and we've seen two, so do action
+        if noFlags(ev) and firstDown and secondDown then
             if module.action then module.action() end
             timeFirstControl, firstDown, secondDown = 0, false, false
-        elseif onlyCtrl(ev) and not firstDown then         -- ctrl down and it's a first
+        elseif onlyCtrl(ev) and not firstDown then
             firstDown = true
             timeFirstControl = timer.secondsSinceEpoch()
-        elseif onlyCtrl(ev) and firstDown then             -- ctrl down and it's the second
+        elseif onlyCtrl(ev) and firstDown then
             secondDown = true
-        elseif not noFlags(ev) then                        -- otherwise reset and start over
+        elseif not noFlags(ev) then
             timeFirstControl, firstDown, secondDown = 0, false, false
         end
-    else -- it was a key press, so not a lone ctrl char -- we don't care about it
+    else
         timeFirstControl, firstDown, secondDown = 0, false, false
     end
     return false
 end):start()
-
-return module
