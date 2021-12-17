b291075618a231a63d4d417e1624b6e85a4a69bb
m-hamashita <masakatsu.hamashita@gunosy.com>
2021-12-17 23:04:51 +0900

    Add confirm temperature alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -72,6 +72,7 @@ abbr -a .... '../../../'
 abbr -a air 'remo aircon send --name エアコン'
 abbr -a bl 'remo signal send AV機器 bluetooth'
 abbr -a light 'remo signal send light_for_signal'
+abbr -a temp 'sudo powermetrics --samplers smc | grep "CPU die temperature"'
 # abbr -a del "git branch --merged | grep -vE '^\\*|master|develop|staging' | xargs -I % git branch -d % && git remote prune origin"
 
 if [ (command -v rmtrash) ]
