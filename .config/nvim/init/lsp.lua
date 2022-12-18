bb2ab4796522b2894bcdae9e1b08efcf68bb073c
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-12-18 14:23:10 +0900

    Disable format on save

--- .config/nvim/init/lsp.lua
+++ .config/nvim/init/lsp.lua
@@ -67,20 +67,19 @@ end })
 
 
 -- null-ls
-
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
 null_ls.setup {
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr, async = true})
-            end,
-        })
+        -- vim.api.nvim_create_autocmd("BufWritePre", {
+        --     group = augroup,
+        --     buffer = bufnr,
+        --     callback = function()
+        --         vim.lsp.buf.format({ bufnr = bufnr })
+        --     end,
+        -- })
     end
     local bufopts = { buffer = bufnr }
     vim.keymap.set('n', 'gk', function()
