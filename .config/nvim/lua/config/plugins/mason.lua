19270c8f8497130955b9299d70a4d773c403ff37
m-hamashita <masakatsu.hamashita@gunosy.com>
2023-01-03 13:51:53 +0900

    Update plugins

--- .config/nvim/lua/config/plugins/mason.lua
+++ .config/nvim/lua/config/plugins/mason.lua
@@ -22,6 +22,7 @@ function M.config()
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+		vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 	end
 	lspconfig.setup_handlers({
 		function(server_name)
