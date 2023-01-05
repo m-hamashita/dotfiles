49cd3b6e076e772307c1a3e35c74458d54c89a4e
m-hamashita <masakatsu.hamashita@gunosy.com>
2023-01-06 01:34:58 +0900

    Add copilot.vim

--- .config/nvim/lua/config/plugins/copilot.lua
+++ .config/nvim/lua/config/plugins/copilot.lua
@@ -1,19 +1,27 @@
-local status_ok, copilot = pcall(require, "copilot")
-if not status_ok then
-	return
+local M = {}
+
+function M.config()
+    local status_ok, copilot = pcall(require, "copilot")
+    if not status_ok then
+        print("hoge")
+        return
+    end
+
+    local status_c_ok, copilot_cmp = pcall(require, "copilot_cmp")
+    if not status_c_ok then
+        print("fuga")
+        return
+    end
+
+    copilot.setup({
+        cmp = {
+            enabled = true,
+        },
+    })
+
+    copilot_cmp.setup({
+        method = "getCompletionsCycling",
+    })
 end
 
-local status_c_ok, copilot_cmp = pcall(require, "copilot_cmp")
-if not status_c_ok then
-	return
-end
-
-copilot.setup({
-	cmp = {
-		enabled = true,
-	},
-})
-
-copilot_cmp.setup({
-	method = "getCompletionsCycling",
-})
+return M
