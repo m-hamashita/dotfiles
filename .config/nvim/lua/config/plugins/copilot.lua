4496596170460f370d3d909f37659681f4331505
m-hamashita <masakatsu.hamashita@legalontech.jp>
2024-03-04 16:05:30 +0900

    Update for copilot

--- .config/nvim/lua/config/plugins/copilot.lua
+++ .config/nvim/lua/config/plugins/copilot.lua
@@ -1,27 +1,27 @@
 local M = {}
 
 function M.config()
-    local status_ok, copilot = pcall(require, "copilot")
-    if not status_ok then
-        print("hoge")
-        return
-    end
+	local status_ok, copilot = pcall(require, "copilot")
+	if not status_ok then
+		print("hoge")
+		return
+	end
 
-    local status_c_ok, copilot_cmp = pcall(require, "copilot_cmp")
-    if not status_c_ok then
-        print("fuga")
-        return
-    end
+	local status_c_ok, copilot_cmp = pcall(require, "copilot_cmp")
+	if not status_c_ok then
+		print("fuga")
+		return
+	end
 
-    copilot.setup({
-        cmp = {
-            enabled = true,
-        },
-    })
+	copilot.setup({
+		cmp = {
+			enabled = true,
+		},
+	})
 
-    copilot_cmp.setup({
-        method = "getCompletionsCycling",
-    })
+	copilot_cmp.setup({
+		method = "getCompletionsCycling",
+	})
 end
 
 return M
