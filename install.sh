c8c6ce8af0ab6b15a13fe9c45c88a3d425b6f85a
m-hamashita <masakatsu.hamashita@legalontech.jp>
2024-06-15 01:29:02 +0900

    Small fix

--- install.sh
+++ install.sh
@@ -2,27 +2,43 @@
 # tpm install
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# neovim install
+# neovim
 if [ "$(uname)" == 'Darwin' ]; then
     brew install neovim
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    curl -L https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage -o nvim
+    curl -L https://github.com/neovim/neovim/releases/download/v0.10.0/nvim.appimage -o nvim
     mkdir -p ~/local/bin/
     mv ./nvim ~/local/bin/nvim
     rm nvim
 fi
 
-# fish install
-brew install fish
-echo "Please add `/usr/local/bin/fish` to /etc/shells "
+# fish
+if [ "$(uname)" == 'Darwin' ]; then
+    brew install fish
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    # TODO: debian 12 only
+    echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+    curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+    sudo apt update
+    sudo apt install fish
+fi
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# for symlink
+rm -rf ~/.config/fish
 
 # font install
 echo "Please install powerline font"
-echo "Font: menlo for powerline"
+echo "Font: Meslo for powerline"
 echo "Non-ASCII: Hack Nerd Font Mono for defx.nvim"
+if [ "$(uname)" == 'Darwin' ]; then
+    brew install font-hack-nerd-font
+fi
 
-chsh -s /usr/local/bin/fish
+echo "Please add `/usr/local/bin/fish` to /etc/shells"
+# chsh -s /usr/local/bin/fish
 
 # aqua install
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.3.0/aqua-installer | bash
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer | bash
+
+mkdir ~/.local/share/aquaproj-aqua
