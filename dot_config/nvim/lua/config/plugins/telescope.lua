635ea5a7c8543e95b7ce843ad01fda88adfd5fde
m-hamashita <masakatsu.hamashita@legalontech.jp>
2025-04-13 01:25:39 +0900

    Update plugins for neovim

--- dot_config/nvim/lua/config/plugins/telescope.lua
+++ dot_config/nvim/lua/config/plugins/telescope.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 function M.config()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>gg", builtin.git_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	local builtin = require("telescope.builtin")
+	vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+	vim.keymap.set("n", "<leader>gg", builtin.git_files, {})
+	vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+	vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+	vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 end
 
 return M
