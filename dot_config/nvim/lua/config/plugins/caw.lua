e2d1388b92f9b74f0e52b99e483f09a050e0306a
m-hamashita <masakatsu.hamashita@legalontech.jp>
2025-04-13 01:51:07 +0900

    Delete unnecessary lua config

--- dot_config/nvim/lua/config/plugins/caw.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-function M.config()
-    vim.keymap.set("n", ",", "<Plug>(caw:hatpos:toggle)")
-    vim.keymap.set("v", ",", "<Plug>(caw:hatpos:toggle)")
-end
-
-return M
--- dot_config/nvim/lua/config/plugins/vim-pydocstring.lua
+++
@@ -1,9 +0,0 @@
--- うまくいってないかも
-local M = {}
-
-function M.config()
-    vim.g.pydocstring_formatter = "google"
-    vim.keymap.set("n", "<leader>pd", "<Plug>(pydocstring)")
-end
-
-return M
