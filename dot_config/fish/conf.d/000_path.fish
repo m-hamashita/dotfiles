695799ce88cebdfc24c0296f0f93a94231d48a1d
m-hamashita <masakatsu.hamashita@legalontech.jp>
2025-06-02 12:27:59 +0900

    Update dotfiles

--- dot_config/fish/conf.d/000_path.fish
+++ dot_config/fish/conf.d/000_path.fish
@@ -46,6 +46,9 @@ set -x AQUA_DISABLE_POLICY true  # TODO: AQUA_DISABLE_POLICY false
 set -x AQUA_POLICY_CONFIG ~/.local/share/aquaproj-aqua/aqua-policy.yaml
 set -x XDG_CONFIG_HOME $HOME/.config
 
+set -x HOMEBREW_NO_AUTO_UPDATE 1
+set -x HOMEBREW_NO_INSTALL_CLEANUP 1
+
 # tfenv TODO: make it good
 set -x PATH $HOME/.local/share/aquaproj-aqua/pkgs/github_archive/github.com/tfutils/tfenv/v3.0.0/tfenv-3.0.0/bin $PATH
 
