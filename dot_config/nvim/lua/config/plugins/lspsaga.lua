c1e1f6bb177d0e0c3bd2ba808d82879fcfe8d63d
m-hamashita <masakatsu.hamashita@legalontech.jp>
2026-03-05 16:14:21 +0900

    Update

--- dot_config/nvim/lua/config/plugins/lspsaga.lua
+++ dot_config/nvim/lua/config/plugins/lspsaga.lua
@@ -1,12 +1,19 @@
 local M = {}
 
 function M.config()
-    local lspsaga = require("lspsaga")
-    lspsaga.setup({})
-    vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>")
-    vim.keymap.set("n", "gd", "<Cmd>Lspsaga goto_definition<CR>")
-    vim.keymap.set("n", "fd", "<Cmd>Lspsaga finder tyd+ref+imp+def<CR>")
-    vim.keymap.set("n", "<leader>r", "<Cmd>Lspsaga rename<CR>")
+	local lspsaga = require("lspsaga")
+	lspsaga.setup({
+		lightbulb = {
+			enable = false,
+			enable_in_insert = false,
+			sign = false,
+			virtual_text = false,
+		},
+	})
+	vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>")
+	vim.keymap.set("n", "gd", "<Cmd>Lspsaga goto_definition<CR>")
+	vim.keymap.set("n", "fd", "<Cmd>Lspsaga finder tyd+ref+imp+def<CR>")
+	vim.keymap.set("n", "<leader>r", "<Cmd>Lspsaga rename<CR>")
 end
 
 return M
