d80457384683c7fb3593e9d0cf58f80cfa73d0f4
m-hamashita <masakatsu.hamashita@legalontech.jp>
2026-03-07 01:14:30 +0900

    Update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -23,3 +23,10 @@ function man
 			man "$argv[1]"
 end
 
+
+# pnpm
+set -gx PNPM_HOME "/Users/masakatsu.hamashita/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
