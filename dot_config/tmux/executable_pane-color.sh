bb99272f5736e927d32189c906670410b0061cba
m-hamashita <masakatsu.hamashita@gmail.com>
2026-09-04 14:11:09 +0900

    Update

--- dot_config/tmux/executable_pane-color.sh
+++ dot_config/tmux/executable_pane-color.sh
@@ -5,23 +5,61 @@ pane_id="$1"
 [ -n "$pane_id" ] || exit 0
 
 colors="
-#282828
-#242a2e
-#2a262f
-#262d25
-#302822
-#222b2f
-#2d2630
-#293024
-#302a24
-#242630
-#2f2527
-#23302d
+#2b3a3a
+#49362b
+#333954
+#34452b
+#443552
+#4b3f2c
+#28414a
+#4a3334
+#3a3248
+#3b4630
+#2d4a45
+#52372f
+#30384a
+#4d3145
+#434024
+#3f3150
+#2c4438
+#432f3a
+#2f3f52
 "
 
-pane_number="${pane_id#%}"
-color_count=$(printf '%s\n' "$colors" | sed '/^$/d' | wc -l | tr -d ' ')
-color_index=$((pane_number % color_count + 1))
-color=$(printf '%s\n' "$colors" | sed '/^$/d' | sed -n "${color_index}p")
+window_id=$(tmux display-message -p -t "$pane_id" '#{window_id}' 2>/dev/null) || exit 0
+pane_number=$(tmux display-message -p -t "$pane_id" '#{pane_index}' 2>/dev/null) || exit 0
 
-tmux select-pane -t "$pane_id" -P "bg=$color"
+case "$pane_number" in
+    ''|*[!0-9]*) pane_number="${pane_id#%}" ;;
+esac
+
+color_count=$(printf '%s\n' "$colors" | sed '/^$/d' | wc -l | tr -d ' ')
+nearby_colors=$(
+    tmux list-panes -t "$window_id" -F '#{pane_id} #{pane_index} #{pane_bg}' |
+    while read -r other_pane other_index other_bg; do
+        [ "$other_pane" != "$pane_id" ] || continue
+        [ "$other_bg" != "default" ] || continue
+
+        distance=$((other_index - pane_number))
+        [ "$distance" -ge 0 ] || distance=$((0 - distance))
+        [ "$distance" -le 2 ] || continue
+
+        printf '%s\n' "$other_bg"
+    done
+)
+
+offset=0
+while [ "$offset" -lt "$color_count" ]; do
+    color_index=$(((pane_number + offset * 7) % color_count + 1))
+    color=$(printf '%s\n' "$colors" | sed '/^$/d' | sed -n "${color_index}p")
+
+    if ! printf '%s\n' "$nearby_colors" | grep -qxF "$color"; then
+        break
+    fi
+
+    offset=$((offset + 1))
+done
+
+# Pane-scoped options avoid select-pane focus changes.
+tmux set-option -pt "$pane_id" window-style "bg=$color" 2>/dev/null
+tmux set-option -pt "$pane_id" window-active-style "bg=$color" 2>/dev/null
